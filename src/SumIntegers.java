52097ac9707914488e037c415e9c07d0f3f09fe1
jeevan1133 <jeevan.basnet@selu.edu>
2018-10-05 14:22:13 -0700

    adding files

---
+++ src/SumIntegers.java
@@ -0,0 +1,14 @@
+
+public class SumIntegers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a_num = 20;
+		int b_num = 40;
+		System.out.println("First number is " + a_num+ " second number is "+ b_num+".");
+		
+		int c_num = a_num + b_num;
+		System.out.println("The sum is " + c_num+ ".");
+	}
+
+}
